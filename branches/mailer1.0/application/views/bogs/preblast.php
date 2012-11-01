821ce39302d968f2f524dcb75c7b1eb3ff61d522
isbogs@gmail.com <isbogs@gmail.com@1a7b0647-876c-e57a-0472-d26faabe3984>
2012-11-01 14:15:11 +0000

    new view for bogs normal blast (no template)

---
+++ branches/mailer1.0/application/views/bogs/preblast.php
@@ -0,0 +1,24 @@
+<html>
+<head></head>
+<body>
+	<form method="post" action="blast">
+					<table>
+						<tr>
+							<td>Title</td>
+							<td><input name="title" type="text" maxlength="100"/></td>
+						</tr>
+						<tr>
+							<td>Message</td>
+							<td><textarea name="html_message" type="textarea" cols="30" rows="10"></textarea></td>
+						</tr>
+						<tr>
+							<td align="center" colspan="2">
+							<input type="submit" value="Submit" />
+							</td>
+						</tr>
+					</table>
+				</form>
+</body>
+
+
+</html>
