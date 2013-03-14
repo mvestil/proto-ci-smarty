7141035c42b4241b10477253cc104f2cd1043dd7
isbogs@gmail.com <isbogs@gmail.com@1a7b0647-876c-e57a-0472-d26faabe3984>
2013-03-14 22:47:12 +0000

    new templates for Money4You user

---
+++ application/views/bogs/moneyforyoutemplate.php
@@ -0,0 +1,108 @@
+<table width="730px" style="background:#353c40 url(https://www.balluuntem.com/platform/email/ba-bg.jpg) 0 0 no-repeat;margin:0;padding:30px" cellspacing="0" cellpadding="0">
+   <tbody>
+      <tr>
+         <td>
+            <table style="width:710px;background:#fff;font:14px/1.571em Helvetica,Arial,sans-serif;color:#253135;overflow:hidden" cellspacing="0" cellpadding="0">
+               <tbody>
+                  <tr>
+                     <td height="70" style="background-color:#f4f5f5;min-height:70px;text-align:center;border-bottom:1px solid #f0f1f3">
+                        <a href="" style="text-decoration:none;font-size:22px;color:#888bae;font-weight:bold;border-bottom:0px solid #e4e6ec" target="_blank">Top Income Generators</a>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td style="background-color:#ffffff;padding-bottom:50px">
+                        <!--<table width="100%" cellspacing="0" cellpadding="0">
+                           <tbody>
+                              <tr>
+                                 <td style="font-size:20px;color:#2c2c2c;padding:20px 33px">New Collections</td>
+                                 <td align="right" style="font-size:12px;color:#666666;padding:20px 33px">Available for ordering now</td>
+                              </tr>
+                           </tbody>
+                           </table>-->
+                        <table align="center" border="0" cellpadding="5" cellspacing="2" style="width:500px; padding-bottom:30px;">
+                           <tbody>
+                              <tr>
+                                 <td style="text-align:center"><a href="http://www.zegolia.com/?ref=isbogs" target="_blank"><span style="font-size:12px">Zegolia</span></a></td>
+                                 <td style="text-align:center"><a href="http://adsexplosive.net/?ref=isbogs" target="_blank"><span style="font-size:12px">Adsexplosive</span></a></td>
+                                 <td style="text-align:center"><a href="http://www.hourlyrevshare.net/?ref=musica" target="_blank"><span style="font-size:12px">HRS</span></a></td>
+                              </tr>
+                              <tr>
+                                 <td style="width:166px;text-align:center"><a href="http://www.zegolia.com/?ref=isbogs" target="_blank"><img width="125px" height="125px" src="http://www.zegolia.com/banners/ban1.gif"></a></td>
+                                 <td style="width:166px;text-align:center"><a href="http://adsexplosive.net/?ref=isbogs" target="_blank"><img width="125px" height="125px" src="http://adsexplosive.net/banners/ban1.gif"></a></td>
+                                 <td style="width:166px;text-align:center"><a href="http://www.hourlyrevshare.net/?ref=musica" target="_blank"><img width="125px" height="125px" src="http://www.hourlyrevshare.net/banners/ban1.gif"></a></td>
+                              </tr>
+                           </tbody>
+                        </table>
+                        <div id="main_content" style="border:1px solid #e4e6ec;margin-left:33px;margin-right:30px;margin-bottom:33px;padding:33px;font-size:14px;color:#666666">
+                          <?php echo $message;?>
+                        </div>
+                        <div align="center" style="clear:both;margin:0;padding:0;list-style:none"><a href="<?php echo $reflink; ?>" style="display:inline-block;width:250px;min-height:50px;line-height:50px;margin-left:0px;margin-bottom:33px;background:#33393d;color:white;text-align:center;text-decoration:none;font-size:18px;font-weight:bold" target="_blank">JOIN NOW</a></div>
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <div style="line-height:1;padding:15px 20px;background:#e6e9ed;overflow:hidden;clear:both">
+                           <p>Prelaunch Programs</p>
+                           <table align="center" border="0" cellpadding="5" cellspacing="2" style="width:500px; padding-bottom:30px;">
+                              <tbody>
+                                 <tr>
+                                    <td style="text-align:center"><a href="http://www.zegolia.com/?ref=isbogs" target="_blank"><span style="font-size:12px">Zegolia</span></a></td>
+                                    <td style="text-align:center"><a href="http://adsexplosive.net/?ref=isbogs" target="_blank"><span style="font-size:12px">Adsexplosive</span></a></td>
+                                    <td style="text-align:center"><a href="http://www.hourlyrevshare.net/?ref=musica" target="_blank"><span style="font-size:12px">HRS</span></a></td>
+                                 </tr>
+                                 <tr>
+                                    <td style="width:166px;text-align:center"><a href="http://www.zegolia.com/?ref=isbogs" target="_blank"><img width="125px" height="125px" src="http://www.zegolia.com/banners/ban1.gif"></a></td>
+                                    <td style="width:166px;text-align:center"><a href="http://adsexplosive.net/?ref=isbogs" target="_blank"><img width="125px" height="125px" src="http://adsexplosive.net/banners/ban1.gif"></a></td>
+                                    <td style="width:166px;text-align:center"><a href="http://www.hourlyrevshare.net/?ref=musica" target="_blank"><img width="125px" height="125px" src="http://www.hourlyrevshare.net/banners/ban1.gif"></a></td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </div>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+            <div align="center">
+               <table width="600" cellspacing="0" cellpadding="0" border="0">
+                  <tbody>
+                     <tr>
+                        <td bgcolor="" align="center" style="background-color:">
+                           <div>
+                              <table width="600" cellspacing="0" cellpadding="0" border="0">
+                                 <tbody>
+                                    <tr>
+                                       <td width="100.0%" valign="top" style="width:100.0%;text-align:left;padding-top:20px;font-family:sans-serif;font-size:15px;line-height:1.5em;color:#444444" colspan="12">
+                                          <div>
+                                             <p style="margin-bottom:1em;font-family:Geneva,Verdana,Arial,Helvetica,sans-serif;text-align:center;font-size:12px;line-height:1.34em;color:#999999;display:block">
+                                                This message is not a SPAM. I have no intention to spam anyone.<br/><br/>You were submitted to my mailing list because you:<br><br>
+                                                1) are a professional online marketer
+                                                2) sent me a business opportunity
+                                                3) had expressed an interest in receiving information about online opportunities
+                                                4) or were on an opt-in safe list.
+                                             </p>
+                                          </div>
+                                       </td>
+                                    </tr>
+                                 </tbody>
+                              </table>
+                           </div>
+                        </td>
+                     </tr>
+                     <tr>
+                        <td style="padding:10px 30px">
+                           <!--<p style="margin-bottom:1em;font-family:Geneva,Verdana,Arial,Helvetica,sans-serif;text-align:center;font-size:9px;line-height:1.34em;color:#999999;display:block">
+                              <a href="" style="text-decoration:none" target="_blank"><span style="color:#999999">Happy Earnings :)</span></a>
+                              </p>
+                              <br>-->
+                           <p style="margin-bottom:1em;font-family:Geneva,Verdana,Arial,Helvetica,sans-serif;text-align:center;font-size:9px;line-height:1.34em;color:#999999;display:block">
+                              If you do want to receive email notification of this kind, ask to be removed.
+                           </p>
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+         </td>
+      </tr>
+   </tbody>
+</table>
